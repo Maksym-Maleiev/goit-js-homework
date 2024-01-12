f3056b50fe93dec1890f272f799364b7deeef419
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-12 16:41:58 +0200

    Update 01-task.js

--- src/js/01-task.js
+++ src/js/01-task.js
@@ -6,12 +6,3 @@
 const numbers = [51, 18, 13, 24, 7, 85, 19];
 let smallestNumber = numbers[0];
 let message;
-
-for (const number of numbers) {
-  if (number < smallestNumber) {
-    smallestNumber = number;
-    message = `Число ${number} є найменшим`;
-  }
-}
-
-console.log(message);

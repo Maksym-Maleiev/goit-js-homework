9c18e428fbd5edd8ef26d0014815223c3e569f0f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-12 16:49:04 +0200

    Update 01-task.js

--- src/js/01-task.js
+++ src/js/01-task.js
@@ -3,13 +3,13 @@
  * за умови, що числа унікальні (не повторюються).
  */
 
-const numbers = [51, 18, 13, 24, 7, 85, 19];
+const numbers = [51, 18, 104, 24, 7, 85, 19];
 let biggestNumber = numbers[0];
 let message;
 
 for (const number of numbers) {
   console.log(number);
-  
+
   if (number > biggestNumber) {
     biggestNumber = number;
     message = `Число ${number} є найбільшим`;

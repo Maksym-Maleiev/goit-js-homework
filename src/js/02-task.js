c0e65ce4a463e0e0ecfcc195f589ae274c814205
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-16 12:01:04 +0200

    Update 02-task.js

--- src/js/02-task.js
+++ src/js/02-task.js
@@ -18,7 +18,7 @@ for (let i = 0; i < strArr.length; i += 1){
   // const join = reversed.join('');
   // console.log(join);
 
-  strArr[i].split('').reverse()
+  strArr[i].split('').reverse().join('')
 
   result += join;
 }
